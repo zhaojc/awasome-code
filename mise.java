ed89a8462809ad962ac78c64a45816dba063f933
zhaojc <zhaojichengreal@gmail.com>
2025-04-24 11:07:31 +0800

    Update mise.java

--- mise.java
+++ mise.java
@@ -60,3 +60,79 @@ public class QueryParameters {
                 .collect(Collectors.joining(", ", "{", "}"));
     }
 }
+
+//动态条件构建
+public class FilterExpressionBuilder {
+
+	public Op eq(String key, Object value) {
+		return new Op(new Filter.Expression(ExpressionType.EQ, new Key(key), new Value(value)));
+	}
+
+	public Op ne(String key, Object value) {
+		return new Op(new Filter.Expression(ExpressionType.NE, new Key(key), new Value(value)));
+	}
+
+	public Op gt(String key, Object value) {
+		return new Op(new Filter.Expression(ExpressionType.GT, new Key(key), new Value(value)));
+	}
+
+	public Op gte(String key, Object value) {
+		return new Op(new Filter.Expression(ExpressionType.GTE, new Key(key), new Value(value)));
+	}
+
+	public Op lt(String key, Object value) {
+		return new Op(new Filter.Expression(ExpressionType.LT, new Key(key), new Value(value)));
+	}
+
+	public Op lte(String key, Object value) {
+		return new Op(new Filter.Expression(ExpressionType.LTE, new Key(key), new Value(value)));
+	}
+
+	public Op and(Op left, Op right) {
+		return new Op(new Filter.Expression(ExpressionType.AND, left.expression, right.expression));
+	}
+
+	public Op or(Op left, Op right) {
+		return new Op(new Filter.Expression(ExpressionType.OR, left.expression, right.expression));
+	}
+
+	public Op in(String key, Object... values) {
+		return this.in(key, List.of(values));
+	}
+
+	public Op in(String key, List<Object> values) {
+		return new Op(new Filter.Expression(ExpressionType.IN, new Key(key), new Value(values)));
+	}
+
+	public Op nin(String key, Object... values) {
+		return this.nin(key, List.of(values));
+	}
+
+	public Op nin(String key, List<Object> values) {
+		return new Op(new Filter.Expression(ExpressionType.NIN, new Key(key), new Value(values)));
+	}
+
+	public Op group(Op content) {
+		return new Op(new Filter.Group(content.build()));
+	}
+
+	public Op not(Op content) {
+		return new Op(new Filter.Expression(ExpressionType.NOT, content.expression, null));
+	}
+
+	public record Op(Filter.Operand expression) {
+
+		public Filter.Expression build() {
+			if (this.expression instanceof Filter.Group group) {
+				// Remove the top-level grouping.
+				return group.content();
+			}
+			else if (this.expression instanceof Filter.Expression exp) {
+				return exp;
+			}
+			throw new RuntimeException("Invalid expression: " + this.expression);
+		}
+
+	}
+
+}
