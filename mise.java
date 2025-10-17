e8a4b73ec1a450066708c92b335c464f77cc3c4b
zhaojc <zhaojichengreal@gmail.com>
2025-10-17 11:24:28 +0800

    Update mise.java

--- mise.java
+++ mise.java
@@ -321,3 +321,179 @@ public class RetryUtility {
         }
     }
 }
+
+//spring configuration类先执行autowrited再执行初始化bean
+
+
+@Configuration(proxyBeanMethods = false)
+public class WebSecurityConfiguration implements ImportAware, BeanClassLoaderAware {
+
+	private WebSecurity webSecurity;
+
+	private Boolean debugEnabled;
+
+	private List<SecurityConfigurer<Filter, WebSecurity>> webSecurityConfigurers;
+
+	private List<SecurityFilterChain> securityFilterChains = Collections.emptyList();
+
+	private List<WebSecurityCustomizer> webSecurityCustomizers = Collections.emptyList();
+
+	private ClassLoader beanClassLoader;
+
+	@Autowired(required = false)
+	private ObjectPostProcessor<Object> objectObjectPostProcessor;
+
+	@Autowired(required = false)
+	private HttpSecurity httpSecurity;
+
+	@Bean
+	public static DelegatingApplicationListener delegatingApplicationListener() {
+		return new DelegatingApplicationListener();
+	}
+
+	@Bean
+	@DependsOn(AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME)
+	public SecurityExpressionHandler<FilterInvocation> webSecurityExpressionHandler() {
+		return this.webSecurity.getExpressionHandler();
+	}
+
+	/**
+	 * Creates the Spring Security Filter Chain
+	 * @return the {@link Filter} that represents the security filter chain
+	 * @throws Exception
+	 */
+	@Bean(name = AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME)
+	public Filter springSecurityFilterChain() throws Exception {
+		boolean hasFilterChain = !this.securityFilterChains.isEmpty();
+		if (!hasFilterChain) {
+			this.webSecurity.addSecurityFilterChainBuilder(() -> {
+				this.httpSecurity.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());
+				this.httpSecurity.formLogin(Customizer.withDefaults());
+				this.httpSecurity.httpBasic(Customizer.withDefaults());
+				return this.httpSecurity.build();
+			});
+		}
+		for (SecurityFilterChain securityFilterChain : this.securityFilterChains) {
+			this.webSecurity.addSecurityFilterChainBuilder(() -> securityFilterChain);
+		}
+		for (WebSecurityCustomizer customizer : this.webSecurityCustomizers) {
+			customizer.customize(this.webSecurity);
+		}
+		return this.webSecurity.build();
+	}
+
+	/**
+	 * Creates the {@link WebInvocationPrivilegeEvaluator} that is necessary to evaluate
+	 * privileges for a given web URI
+	 * @return the {@link WebInvocationPrivilegeEvaluator}
+	 */
+	@Bean
+	@DependsOn(AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME)
+	public WebInvocationPrivilegeEvaluator privilegeEvaluator() {
+		return this.webSecurity.getPrivilegeEvaluator();
+	}
+
+	/**
+	 * Sets the {@code <SecurityConfigurer<FilterChainProxy, WebSecurityBuilder>}
+	 * instances used to create the web configuration.
+	 * @param objectPostProcessor the {@link ObjectPostProcessor} used to create a
+	 * {@link WebSecurity} instance
+	 * @param beanFactory the bean factory to use to retrieve the relevant
+	 * {@code <SecurityConfigurer<FilterChainProxy, WebSecurityBuilder>} instances used to
+	 * create the web configuration
+	 * @throws Exception
+	 */
+	@Autowired(required = false)
+	public void setFilterChainProxySecurityConfigurer(ObjectPostProcessor<Object> objectPostProcessor,
+			ConfigurableListableBeanFactory beanFactory) throws Exception {
+		this.webSecurity = objectPostProcessor.postProcess(new WebSecurity(objectPostProcessor));
+		if (this.debugEnabled != null) {
+			this.webSecurity.debug(this.debugEnabled);
+		}
+		List<SecurityConfigurer<Filter, WebSecurity>> webSecurityConfigurers = new AutowiredWebSecurityConfigurersIgnoreParents(
+				beanFactory)
+			.getWebSecurityConfigurers();
+		webSecurityConfigurers.sort(AnnotationAwareOrderComparator.INSTANCE);
+		Integer previousOrder = null;
+		Object previousConfig = null;
+		for (SecurityConfigurer<Filter, WebSecurity> config : webSecurityConfigurers) {
+			Integer order = AnnotationAwareOrderComparator.lookupOrder(config);
+			if (previousOrder != null && previousOrder.equals(order)) {
+				throw new IllegalStateException("@Order on WebSecurityConfigurers must be unique. Order of " + order
+						+ " was already used on " + previousConfig + ", so it cannot be used on " + config + " too.");
+			}
+			previousOrder = order;
+			previousConfig = config;
+		}
+		for (SecurityConfigurer<Filter, WebSecurity> webSecurityConfigurer : webSecurityConfigurers) {
+			this.webSecurity.apply(webSecurityConfigurer);
+		}
+		this.webSecurityConfigurers = webSecurityConfigurers;
+	}
+
+	@Autowired(required = false)
+	void setFilterChains(List<SecurityFilterChain> securityFilterChains) {
+		this.securityFilterChains = securityFilterChains;
+	}
+
+	@Autowired(required = false)
+	void setWebSecurityCustomizers(List<WebSecurityCustomizer> webSecurityCustomizers) {
+		this.webSecurityCustomizers = webSecurityCustomizers;
+	}
+
+	@Bean
+	public static BeanFactoryPostProcessor conversionServicePostProcessor() {
+		return new RsaKeyConversionServicePostProcessor();
+	}
+
+	@Override
+	public void setImportMetadata(AnnotationMetadata importMetadata) {
+		Map<String, Object> enableWebSecurityAttrMap = importMetadata
+			.getAnnotationAttributes(EnableWebSecurity.class.getName());
+		AnnotationAttributes enableWebSecurityAttrs = AnnotationAttributes.fromMap(enableWebSecurityAttrMap);
+		this.debugEnabled = enableWebSecurityAttrs.getBoolean("debug");
+		if (this.webSecurity != null) {
+			this.webSecurity.debug(this.debugEnabled);
+		}
+	}
+
+	@Override
+	public void setBeanClassLoader(ClassLoader classLoader) {
+		this.beanClassLoader = classLoader;
+	}
+
+	/**
+	 * A custom version of the Spring provided AnnotationAwareOrderComparator that uses
+	 * {@link AnnotationUtils#findAnnotation(Class, Class)} to look on super class
+	 * instances for the {@link Order} annotation.
+	 *
+	 * @author Rob Winch
+	 * @since 3.2
+	 */
+	private static class AnnotationAwareOrderComparator extends OrderComparator {
+
+		private static final AnnotationAwareOrderComparator INSTANCE = new AnnotationAwareOrderComparator();
+
+		@Override
+		protected int getOrder(Object obj) {
+			return lookupOrder(obj);
+		}
+
+		private static int lookupOrder(Object obj) {
+			if (obj instanceof Ordered) {
+				return ((Ordered) obj).getOrder();
+			}
+			if (obj != null) {
+				Class<?> clazz = ((obj instanceof Class) ? (Class<?>) obj : obj.getClass());
+				Order order = AnnotationUtils.findAnnotation(clazz, Order.class);
+				if (order != null) {
+					return order.value();
+				}
+			}
+			return Ordered.LOWEST_PRECEDENCE;
+		}
+
+	}
+
+}
+
